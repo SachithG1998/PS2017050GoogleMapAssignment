f817452edfa308b16721a7a54a0cb250aafa3f21
sachith-gunasekara <sachithgunasekara@outlook.com>
2022-05-16 02:11:56 +0530

    Able to select a university from the dropdown. The map will display that specific location.

--- app/src/main/java/lk/ac/kln/stu/ps2017050googlemapassignment/MainActivity.kt
+++ app/src/main/java/lk/ac/kln/stu/ps2017050googlemapassignment/MainActivity.kt
@@ -2,10 +2,26 @@ package lk.ac.kln.stu.ps2017050googlemapassignment
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupActionBarWithNavController
+import lk.ac.kln.stu.ps2017050googlemapassignment.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private var _binding: ActivityMainBinding? = null
+
+    private val binding get() = _binding!!
+    private lateinit var navController: NavController
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        _binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        navController = navHostFragment.navController
+
+        setupActionBarWithNavController(navController)
     }
 }
---
+++ app/src/main/java/lk/ac/kln/stu/ps2017050googlemapassignment/model/University.kt
@@ -0,0 +1,5 @@
+package lk.ac.kln.stu.ps2017050googlemapassignment.model
+
+import com.google.android.gms.maps.model.LatLng
+
+data class University(val name: String, val location: LatLng)
